9fc9a2a0a49a58f806387317b600edcedebcdf25
ilmarcopardo <marco.pardini00@gmail.com>
2024-11-19 21:45:24 +0100

    added post webrtc

---
+++ functioning_webrtc/client.js
@@ -0,0 +1,136 @@
+// get DOM elements
+var dataChannelLog = document.getElementById('data-channel'),
+    iceConnectionLog = document.getElementById('ice-connection-state'),
+    iceGatheringLog = document.getElementById('ice-gathering-state'),
+    signalingLog = document.getElementById('signaling-state');
+
+// peer connection
+var pc = null;
+
+// data channel
+var dc = null, dcInterval = null;
+
+function createPeerConnection() {
+    var config = {
+        iceServers : [{ urls: ['stun:stun.l.google.com:19302'] }]
+    };
+
+    pc = new RTCPeerConnection(config);
+
+    // register some listeners to help debugging
+    pc.addEventListener('icegatheringstatechange', () => {
+        iceGatheringLog.textContent += ' -> ' + pc.iceGatheringState;
+    }, false);
+    iceGatheringLog.textContent = pc.iceGatheringState;
+
+    pc.addEventListener('iceconnectionstatechange', () => {
+        iceConnectionLog.textContent += ' -> ' + pc.iceConnectionState;
+    }, false);
+    iceConnectionLog.textContent = pc.iceConnectionState;
+
+    pc.addEventListener('signalingstatechange', () => {
+        signalingLog.textContent += ' -> ' + pc.signalingState;
+    }, false);
+    signalingLog.textContent = pc.signalingState;
+    
+    return pc;
+}
+
+function negotiate() {
+    return pc.createOffer().then((offer) => {
+        return pc.setLocalDescription(offer);
+    }).then(() => {
+        // wait for ICE gathering to complete
+        return new Promise((resolve) => {
+            if (pc.iceGatheringState === 'complete') {
+                resolve();
+            } else {
+                function checkState() {
+                    if (pc.iceGatheringState === 'complete') {
+                        pc.removeEventListener('icegatheringstatechange', checkState);
+                        resolve();
+                    }
+                }
+                pc.addEventListener('icegatheringstatechange', checkState);
+            }
+        });
+    }).then(() => {
+        var offer = pc.localDescription;
+        var codec;
+        codec = document.getElementById('video-codec').value;
+       
+        return fetch('/offer', {
+            body: JSON.stringify({
+                sdp: offer.sdp,
+                type: offer.type,
+                video_transform: document.getElementById('video-transform').value
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        });
+    }).then((response) => {
+        return response.json();
+    }).then((answer) => {
+        return pc.setRemoteDescription(answer);
+    }).catch((e) => {
+        alert(e);
+    });
+}
+
+function start() {
+    document.getElementById('start').style.display = 'none';
+
+    pc = createPeerConnection();
+
+    // Build media constraints.
+
+    const constraints = {
+        audio: false,
+        video: true
+    };
+
+    // Acquire media and start negociation.
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        stream.getTracks().forEach((track) => {
+            pc.addTrack(track, stream);
+        });
+        return negotiate();
+    }, (err) => {
+        alert('Could not acquire media: ' + err);
+        });
+
+    document.getElementById('stop').style.display = 'inline-block';
+}
+
+function stop() {
+    document.getElementById('stop').style.display = 'none';
+
+    // close data channel
+    if (dc) {
+        dc.close();
+    }
+
+    // close transceivers
+    if (pc.getTransceivers) {
+        pc.getTransceivers().forEach((transceiver) => {
+            if (transceiver.stop) {
+                transceiver.stop();
+            }
+        });
+    }
+
+    // close local audio / video
+    pc.getSenders().forEach((sender) => {
+        sender.track.stop();
+    });
+
+    // close peer connection
+    setTimeout(() => {
+        pc.close();
+    }, 500);
+}
+
+
